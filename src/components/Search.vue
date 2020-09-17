e7d07f0e1e2e8b2561d1ca3eed1611d81a279906
Jacob <Jacob@DESKTOP-K0OI685>
2020-09-17 10:48:08 -0400

    Initial

---
+++ src/components/Search.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <input v-model="query" type="search" />
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      query: null,
+    };
+  },
+};
+</script>
+
+<style scoped>
+</style>
